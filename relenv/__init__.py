e5896229126e70ebc10b94175ecf770a9f6d6516
MKLeb <calebb@vmware.com>
2022-12-16 17:51:03 -0500

    import relenv version into __init__.py

--- relenv/__init__.py
+++ relenv/__init__.py
@@ -1,2 +1,5 @@
 # Copyright 2022 VMware, Inc.
 # SPDX-License-Identifier: Apache-2
+
+# Importing the version here in the event of a pip install of a local copy of relenv
+from .common import __version__
